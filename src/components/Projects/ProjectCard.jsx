d4efdccf1aaa48ab02b2406523e4efc3febfde70
Vinit Jogi <Vinit.Jogi@ril.com>
2024-08-04 21:03:31 +0530

    projects section completed

---
+++ src/components/Projects/ProjectCard.jsx
@@ -0,0 +1,45 @@
+import React, { useState } from 'react'
+import {getImageUrl} from "../../utils";
+import styles from "./ProjectCard.module.css";
+
+export const ProjectCard = ({ project : {title, imgSrc, description, skills, demo, source} }) => {
+  // const [isReadmore, setReadmore] = useState(true);
+
+  // const toggleReadMore = () => {
+  //   setReadmore(!isReadmore);
+  // }
+  
+  return (
+    <div className={styles.container}>
+      <img
+        className ={styles.image}
+        src={getImageUrl(imgSrc)}
+        alt={`image of ${title}`}
+      />
+      <h3 className ={styles.title}>{title}</h3>
+      <p className ={styles.description}>
+        {description}
+        {/* {console.log(description.slice(0,20))} */}
+        {/* {isReadmore ? description.slice(0, 100) : description}
+        <span
+            onClick={toggleReadMore}
+            className={styles.readOrHide}
+          >
+          {isReadmore ? "...read more" : " show less"}
+        </span> */}
+
+      </p>
+      <ul className ={styles.skills}>
+        {
+          skills.map((skill, id) => {
+            return <li className ={styles.skill} key={id}>{skill}</li>
+          })
+        }
+      </ul>
+      <div className ={styles.links}>
+        <a className ={styles.link} href={source}>Source</a>
+      </div>
+    </div>
+  )
+}
+
