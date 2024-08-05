6182220ea14c05628d836a4cf6de7776ba1376c0
Vinit Jogi <Vinit.Jogi@ril.com>
2024-08-05 10:21:42 +0530

    added type writer effect

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styles from './Hero.module.css';
+import Typewriter from 'typewriter-effect';
 import { getImageUrl } from '../../utils';
 
 export const Hero = () => {
@@ -7,11 +8,25 @@ export const Hero = () => {
     <section className={styles.container}>
       <div className={styles.content}>
         <h1 className={styles.title}>Hi, I'm Vinit !</h1>
-        <p className={styles.description}>
+
+        {/* <p className={styles.description}>
           Hi, I'm Vinit Jogi, a Hyperautomation Engineer at Jio Platforms Ltd. 
           I specialize in backend web development using Nodejs, also have hands on experience on 
           working with Ansible and Shell scripting.
-        </p>
+        </p> */}
+        <div className={styles.description}>
+          <Typewriter className= {styles.description}
+            options={{
+              strings: [
+                "Hi, I'm Vinit Jogi, a Hyperautomation Engineer at Jio Platforms Ltd. I specialize in backend web development using Nodejs. I also have hands-on experience working with Ansible and Shell scripting."
+              ],
+              autoStart: true,
+              loop: false,
+              delay: 40,
+              deleteSpeed: Infinity,
+            }}
+          />
+        </div>
         <a href="mailto:vinit.jpl@gmail.com"  className={styles.contactBtn}>Contact Me</a>
       </div>
 
